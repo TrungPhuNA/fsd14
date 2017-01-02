dd3ae4f413bf3d3cfb7550463226775cba26e045
TrungPhuNa <phupt.humg.94@gmail.com>
2017-01-02 23:43:08 +0700

    home

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function change_size(){
+	//lay doi tuong
+	 document.getElementById("search-form").style.width = "250px";
+}
+
+function outsize(){
+	document.getElementById("search-form").style.width = "200px";
+}
+
+/*menu*/
+// lay doi tuong ul  thu nhat
+var sub1 = document.getElementsByClassName("sub1");
